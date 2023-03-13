2c9afe42ca2717265ae85059cc87ac6a1cba8887
mamiemru <mrunder.s@gmail.com>
2023-03-13 14:31:32 +0100

    pep syntax

--- jpegShare/server.py
+++ jpegShare/server.py
@@ -10,7 +10,7 @@ from flask_restx import Resource
 from flask_cors import CORS
 
 from werkzeug.datastructures import FileStorage
-from service.imagesService   import ImagesService
+from service.imagesService import ImagesService
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,27 +20,30 @@ tickets_namespace = api.namespace('')
 articles_namespace = api.namespace('articles')
 
 attachement_request = api.parser()
-attachement_request.add_argument('file', location='files', type=FileStorage, required=True)
+attachement_request.add_argument(
+    'file', location='files', type=FileStorage, required=True
+)
 
 @tickets_namespace.route('/<string:iso_date_prefix>/<string:image_name>')
 class TicketsNameSpace(Resource):
-    
+
     def get(self, iso_date_prefix, image_name):
         print(f"{iso_date_prefix=}, {image_name=}")
         location = Path("D:\\minio\\ticket") / 'ticket' / iso_date_prefix / image_name
         if os.path.exists(location):
             return send_file(location, mimetype='image/png')
-        return Response(image_name, 404) 
+        return Response(image_name, 404)
 
 @articles_namespace.route('/<string:image_name>')
 class ArticlesNameSpace(Resource):
-    
+
     def get(self, image_name):
         print(f"{image_name=}")
         location = Path("D:\\minio\\ticket") / 'article' / 'articles' / image_name
         if os.path.exists(location):
             return send_file(location, mimetype='image/png')
         return Response(image_name, 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9001)
